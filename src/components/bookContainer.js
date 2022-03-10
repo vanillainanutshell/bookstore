4d673f869d42f327f07f81cbb8c0c6f5259686ed
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-10 15:06:05 -0300

    add books.js content

--- src/components/bookContainer.js
+++ src/components/bookContainer.js
@@ -1,27 +1,26 @@
 import ItemBook from './book';
 import Form from './form';
 
-const Books = () => (
-  <section>
+const Books = () => {
+  const bookList = useSelector((state) => state.booksReducer);
+  return (
     <section>
-      <ItemBook
-        title="Le Petit Prince"
-        author="Antoine de Saint-Exupéry"
-        categories="Novel"
-      />
-      <ItemBook
-        title="The Winter King"
-        author="Bernard Cornwell"
-        categories="Historical Ficticion"
-      />
-      <ItemBook
-        title="The Silmarillion"
-        author="J. R. R. Tolkien"
-        categories="Fantasy"
-      />
+      <section>
+        {
+          bookList.map((book) => (
+            <ItemBook
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              author={book.author}
+              categories={book.categories}
+            />
+          ))
+        }
+      </section>
+      <Form />
     </section>
-    <Form />
-  </section>
-);
+  );
+};
 
 export default Books;
