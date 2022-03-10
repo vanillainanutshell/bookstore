3d7c08ceb6cbdf6e5ce219da937688b82356c12f
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-10 15:11:01 -0300

    change form

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addElement } from '../redux/books/books';
 
-const Form = () => (
-  <div className="form-container">
-    <h1>Add new book</h1>
-    <div className="form-inputs">
-      <input type="text" placeholder="Book title" />
-      <select name="categories" id="categories">
-        <option value="Category 1">Novel</option>
-        <option value="Category 2">Historical Ficticion</option>
-        <option value="Category 3">Fantasy</option>
-      </select>
-      <button type="button">Add book</button>
-    </div>
-  </div>
-);
+const Form = () => {
+  const initialFormState = {
+    title: '', author: '', category: '',
+  };
+
+  const [formState, setFormState] = useState(initialFormState);
+
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: [e.target.value],
+    });
+  };
+
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    document.querySelector('form').reset();
+
+    const newBook = {
+      id: uuidv4(),
+      title: formState.title[0],
+      author: formState.author[0],
+      category: formState.author[0],
+    };
+
+    dispatch(addElement(newBook));
+  };
+
+  return (
+    <form onSubmit={submitBookToStore} className="form-container">
+      <h1>ADD NEW BOOK</h1>
+      <div className="form-inputs">
+        <input name="title" type="text" onChange={handleChange} placeholder="Book Title" required />
+        <input name="author" type="text" onChange={handleChange} placeholder="Book Author" required />
+        <select name="category" id="categories" onChange={handleChange} required>
+          <option value="">Select a category</option>
+          <option value="Category 1">Novel</option>
+          <option value="Category 2">Historical Fictition</option>
+          <option value="Category 3">SCI-FI</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
